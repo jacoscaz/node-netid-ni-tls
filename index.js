ba33430e32897e321e714a54d14158209d71f2b5
Jacopo Scazzosi <jacoposcazzosi@gmail.com>
2016-09-12 01:55:16 +0100

    cleanup

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var AUTH = require('./lib/auth');
 var CACHE = require('./lib/cache');
 var STORE = require('./lib/store');
 
+var debug = require('debug')('express:webid:mware');
+
 function factory () {
 
   var store = null;
 
-  STORE.createStore()
+  STORE.create()
     .then(function (_store) {
       store = _store;
     })
@@ -21,12 +23,21 @@ function factory () {
 
   return function (req, res, next) {
 
+    debug('Begin.');
+
+    if (!store) {
+      debug('Store not yet ready.');
+      next();
+      return;
+    }
+
     var clientCertificate = req.connection.encrypted
       && req.connection.getPeerCertificate();
 
-    AUTH.authenticate(clientCertificate, {cache: cache, store: store})
+    AUTH.authenticate(clientCertificate, store, cache)
       .then(function (auth) {
         req.auth = auth;
+        debug('End.');
         next();
       })
       .catch(next);
@@ -35,29 +46,3 @@ function factory () {
 }
 
 module.exports = factory;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
